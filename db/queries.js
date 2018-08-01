5551c19579fa308e608fb5f8b3cdf87204f02f9f
Josh Robbs <joshrobbs@Zuul.local>
2018-07-31 22:09:10 -0600

    finished queries

--- db/queries.js
+++ db/queries.js
@@ -5,11 +5,24 @@ module.exports = {
         return database('game')
     },
     read(id){
+        return database('game').where('id',id).first()
     },
     create(game){
+        return database('game')
+            .insert(game)
+            .returning('*')
+            .then(record => record[0])
     },
     update(id, game){
+        return database('game')
+            .update('game')
+            .where('id', id)
+            .returning('*')
+            .then(record => record[0])
     },
     delete(id){
+        return database('game')
+            .del()
+            .where('id',id)
     }
 };

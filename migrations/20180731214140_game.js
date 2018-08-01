39d4b8fd91fe9d846952041ddd853e90068dfe11
Josh Robbs <joshrobbs@Zuul.local>
2018-07-31 21:51:47 -0600

    added seeds and migrations

---
+++ migrations/20180731214140_game.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('game', game => {
+    game.increments()
+    game.string('name')
+    game.string('developer')
+    game.float('rating')
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('game')
+};
